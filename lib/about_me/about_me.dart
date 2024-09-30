6fd0776bc094beef41db86eb7f55fd11377aaaa4
TopuFD <topuroy3317@gmail.com>
2024-10-01 02:22:37 +0600

    second commit

---
+++ lib/about_me/about_me.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AboutMe extends StatelessWidget {
+  const AboutMe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+              "This is Topu chandra roy, recently i am starting my new job at bd calling as a junior software engineer")
+        ],
+      ),
+    );
+  }
+}
